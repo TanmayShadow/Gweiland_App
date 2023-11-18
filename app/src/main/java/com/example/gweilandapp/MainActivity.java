1ac595407be878dbf249238715227db1de0a85f3
TanmayShadow <tanmayshindkar1430@gmail.com>
2023-11-18 21:52:50 +0530

    3rd Part done, Filter Done

--- app/src/main/java/com/example/gweilandapp/MainActivity.java
+++ app/src/main/java/com/example/gweilandapp/MainActivity.java
@@ -2,19 +2,17 @@ package com.example.gweilandapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
-import android.graphics.ColorSpace;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,7 +20,7 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 
 import java.text.DecimalFormat;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import retrofit2.Call;
@@ -33,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     ProgressBar progressBar;
+    List<MyModel.Datum> currencyList;
+    recycleAdapter adapter;
+
+    LinearLayout filterLayout;
+    AppCompatButton pbutton,vbutton;
 
 
     @Override
@@ -50,6 +53,10 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView.setLayoutManager(layoutManager);
 
+        pbutton = findViewById(R.id.Price);
+        vbutton = findViewById(R.id.Volume);
+
+
     }
 
     private void listingData()
@@ -61,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<MyModel> call, Response<MyModel> response) {
                 if(response.isSuccessful())
                 {
-                        recycleAdapter adapter = new recycleAdapter(response.body().getData());
+                        adapter = new recycleAdapter(response.body().getData());
                         recyclerView.setAdapter(adapter);
                         progressBar.setVisibility(View.GONE);
                 }
@@ -81,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
 
         public recycleAdapter(List<MyModel.Datum> list) {
             this.list = list;
+            currencyList=list;
         }
 
         @NonNull
@@ -95,12 +103,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onBindViewHolder(@NonNull recycleAdapter.MyViewHolder holder, int position) {
             DecimalFormat df = new DecimalFormat("#.##");
-            Double percentage24Hr = list.get(position).getQuote().getUsd().getPercentChange24h();
-            Double price = list.get(position).getQuote().getUsd().getPrice();
+            Double percentage24Hr = currencyList.get(position).getQuote().getUsd().getPercentChange24h();
+            Double price = currencyList.get(position).getQuote().getUsd().getPrice();
             String percentage = df.format(percentage24Hr);
             String priceFormatted = df.format(price);
-            holder.currencyShortName.setText(list.get(position).getSymbol());
-            holder.currencyName.setText(list.get(position).getName());
+            holder.currencyShortName.setText(currencyList.get(position).getSymbol());
+            holder.currencyName.setText(currencyList.get(position).getName());
             holder.currencyPrice.setText(priceFormatted);
             holder.currencyPercentage.setText(percentage+"%");
 
@@ -110,15 +118,15 @@ public class MainActivity extends AppCompatActivity {
                 holder.currencyPercentage.setTextColor(Color.argb(100,0,255,0));
             }
 
-            String image = "https://s2.coinmarketcap.com/static/img/coins/64x64/"+list.get(position).getId()+".png";
+            String image = "https://s2.coinmarketcap.com/static/img/coins/64x64/"+currencyList.get(position).getId()+".png";
             Picasso.with(getApplicationContext()).load(image).placeholder(R.drawable.sample_logo).fit().into(holder.currencyLogo);
 
         }
 
         @Override
         public int getItemCount() {
-            if(list!=null)
-                return list.size();
+            if(currencyList!=null)
+                return currencyList.size();
             return 0;
         }
 
@@ -140,6 +148,50 @@ public class MainActivity extends AppCompatActivity {
                 currencyLogo = itemView.findViewById(R.id. currencyLogo);
                 currencyChange = itemView.findViewById(R.id.currencyChange);
             }
+        }
+    }
+    class CurrencyComparator implements java.util.Comparator<MyModel.Datum> {
+        @Override
+        public int compare(MyModel.Datum a, MyModel.Datum b) {
+            return  Double.compare(a.getQuote().getUsd().getPrice() , b.getQuote().getUsd().getPrice());
+        }
+    }
+
+    class CurrencyVolumeComparator implements java.util.Comparator<MyModel.Datum> {
+        @Override
+        public int compare(MyModel.Datum a, MyModel.Datum b) {
+            return  Double.compare(a.getQuote().getUsd().getVolume24h() , b.getQuote().getUsd().getVolume24h());
+        }
+    }
+
+
+    public void sortByPrice(View v)
+    {
+        Collections.sort(currencyList, new CurrencyComparator());
+        adapter.notifyDataSetChanged();
+
+        pbutton.setVisibility(View.INVISIBLE);
+        vbutton.setVisibility(View.INVISIBLE);
+    }
+
+    public void sortByVolume(View v)
+    {
+        Collections.sort(currencyList, new CurrencyVolumeComparator());
+        adapter.notifyDataSetChanged();
+        pbutton.setVisibility(View.INVISIBLE);
+        vbutton.setVisibility(View.INVISIBLE);
+    }
+
+    public void showFilter(View v)
+    {
+        if(pbutton.getVisibility()!=View.VISIBLE)
+        {
+            pbutton.setVisibility(View.VISIBLE);
+            vbutton.setVisibility(View.VISIBLE);
+        }
+        else {
+            pbutton.setVisibility(View.INVISIBLE);
+            vbutton.setVisibility(View.INVISIBLE);
         }
     }
 
